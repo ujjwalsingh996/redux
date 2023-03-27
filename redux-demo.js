06ab90b0519044901d100aa831baeb7a7df48a3d
ujjwalsingh996 <ujjwalsingh996@gmail.com>
2023-03-27 18:50:57 +0530

    redux decrement and increment

---
+++ redux-demo.js
@@ -0,0 +1,39 @@
+const redux = require('redux');
+
+const counterReducer = (state = {counter: 0}, action) => {
+    if(action.type === "decrement") {
+        return {
+            counter: state.counter - 1,
+        }
+    }
+    return {
+        counter: state.counter + 1,
+    }
+}
+
+const store = redux.legacy_createStore(counterReducer);
+
+const counterSubscriber = () => {
+    const latestState = store.getState();
+    console.log(latestState);
+}
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({
+    type: 'increment'   
+})
+
+store.dispatch({
+    type: 'increment'
+})
+store.dispatch({
+    type: 'increment'   
+})
+store.dispatch({
+    type: 'increment'   
+})
+
+store.dispatch({
+    type: 'decrement'
+})
